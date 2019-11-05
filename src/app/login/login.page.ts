d44fc341a62e78147cd616016ffdd158839febf1
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2019-11-05 19:44:57 +0000

    Added method that will be called on successful login.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {  
+  constructor(private fb: FormBuilder, private router: Router) {  
     this.loginForm = this.fb.group({
       email: [null, Validators.required],
       password: [null, Validators.required],
@@ -17,6 +18,12 @@ export class LoginPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  signIn() {
+    if (this.loginForm.valid) {
+      this.router.navigate([]);
+    }
   }
 
 }
