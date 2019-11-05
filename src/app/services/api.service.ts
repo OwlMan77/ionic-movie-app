acf59fede8b29d1400bd6a4b2f27367a78c84804
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2019-11-05 21:25:14 +0000

    Defined API service adding a method that will return all movies from the current year.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http' 
+import { environment } from 'src/environments/environment';
+
+import { MovieApiResponse } from 'src/app/models/movie-api-response.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  public postUrl = 'https://image.tmdb.org/t/p/w500/';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private getCurrentYear(): string {
+    return new Date().getFullYear().toString(); 
+  }
+
+  public getMoviesFromThisYear(): Observable<MovieApiResponse> {
+    return this.http.get<MovieApiResponse>(`https://api.themoviedb.org/3/discover/movie?api_key=${environment.movieAPIKey}&language=en-UK&sort_by=popularity.desc&include_adult=false&include_video=false&primary_release_year=${this.getCurrentYear()}`);
+  }
 }
