50fed9dff16a8a480d25a9cf259dbb15b2441d74
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2019-11-06 02:03:51 +0000

    Retrieved movies from API OnInit. Would have liked to use just the async pipe in the template. However HttpClient doesn't seem to emitt a notification unless you use subscribe.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { MovieResult } from '../models/movie-result.model';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public movies: MovieResult[];
 
   constructor(
-    private menu: MenuController
-  ) {}
+    private menu: MenuController,
+    private apiService: ApiService,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.apiService.getMoviesFromThisYear().subscribe((data) => {
+      this.movies = data;
+    })
+  }
 
   toggleMenu(){
     this.menu.toggle('side-menu');
